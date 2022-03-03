da43c3374d7da829569d1a4923c2d99461d09ada
violetwhenisawu <anashape710@gmail.com>
2022-03-03 17:24:35 +0900

    add title, nav

---
+++ app.js
@@ -0,0 +1,14 @@
+// let yOffset = 0;
+
+// function checkMenu() {
+//     if (yOffset > 44) {
+//         document.body.classList.add('local-nav-sticky');
+//     } else {
+//         document.body.classList.remove('local-nav-sticky');
+//     }
+// }
+
+// window.addEventListener('scroll', () => {
+//     yOffset = window.pageYOffset;
+//       checkMenu();
+//   });
